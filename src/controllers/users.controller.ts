e1efdc820ae2657e4643472f561176f157fa12b7
Zameer Ansari <zameer.ansari@instarem.com>
2019-10-20 12:15:13 +0530

    authentication method++

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,5 +1,7 @@
 import * as Express from "express";
 import User from "../database/user.model";
+import * as Crypto from 'crypto';
+import rp from 'request-promise';
 
 const create = async (req: Express.Request, res: Express.Response) => {
     try {
@@ -56,8 +58,43 @@ const findById = async (req: Express.Request, res: Express.Response) => {
     }
 }
 
+const authenticate = async (req: Express.Request, res: Express.Response) => {
+
+    try {
+        const api_key = process.env.ZERODHA_API_KEY;
+        const api_secret = process.env.ZERODHA_API_SECRET;
+        const request_token = req.body.request_token;
+        const checksumInput = `${api_key}${request_token}${api_secret}`;
+        const checksum = await Crypto
+            .createHash('sha256')
+            .update(checksumInput)
+            .digest('hex');
+
+        const options = {
+            url: 'https://api.kite.trade/session/token',
+            method: 'POST',
+            headers: {
+                "X-Kite-Version": 3,
+            },
+            data: {
+                "api_key": api_key,
+                "request_token": request_token,
+                "checksum": checksum,
+            },
+        };
+
+        const accessTokenResponse = await rp(options);
+            console.log(accessTokenResponse);
+        res.send(accessTokenResponse);
+    }
+    catch (ex) {
+        res.send(ex);
+    }
+}
+
 export default {
     findAll,
     findById,
-    create
+    create,
+    authenticate
 }
