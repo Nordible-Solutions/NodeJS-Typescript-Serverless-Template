9b127db11816a14e9c601a9240f0b6609430684e
Zameer Ansari <zameer.ansari@instarem.com>
2019-10-20 13:22:17 +0530

    removed unnecessary routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,5 @@ import * as Express from "express";
 const router = Express.Router();
 import UserController from "./controllers/users.controller";
 
-router.get("/users", UserController.findAll);
-router.get("/users/:id", UserController.findById);
-router.post("/users", UserController.create);
 router.post("/authenticate", UserController.authenticate);
 export default router;
